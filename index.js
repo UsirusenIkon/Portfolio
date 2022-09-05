46354db3fb835abf594caaeaf2007bbc04032063
Emre Karayaman <karayaman.emre@outlook.com>
2022-09-05 16:43:56 +0300

    add disable scrolling

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.nav-menu');
 const links = document.querySelectorAll('.nav-link');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
-  nav.classList.toggle('slide')
-  hamburger.classList.toggle('slide')
-})
+  nav.classList.toggle('slide');
+  hamburger.classList.toggle('slide');
+  body.classList.toggle('overflow');
+});
 
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('slide');
+    hamburger.classList.remove('slide');
+    body.classList.toggle('overflow');
+  });
+});
